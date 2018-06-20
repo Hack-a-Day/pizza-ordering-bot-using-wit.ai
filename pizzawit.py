43cd4e02e80a06239bfca427f780295c5a36989c
stevendpe <stevend@rimstar.org>
2018-06-19 21:46:02 -0400

    Link to Hackaday article

--- pizzawit.py
+++ pizzawit.py
@@ -1,7 +1,7 @@
 # pizzawit - Talks to wit.ai for speech recognition and analysis in order
 # to make a pizza-ordering chatbot. No actual phone call is made.
 #
-# See here for details: # https://hackaday.com/?p=312908
+# See here for details: https://hackaday.com/?p=312908
 #
 # Copyright (c) 2018 - Steven Dufresne
 #
